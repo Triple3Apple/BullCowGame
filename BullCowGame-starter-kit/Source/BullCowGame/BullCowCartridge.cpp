e70ed11d8e979733239dc8e585ce315151b585f6
Abrar Akhand <Jahin345@gmail.com>
2020-01-02 17:17:59 -0700

    small editor changes

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,9 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "BullCowCartridge.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+    Super::BeginPlay(); // needed to make game run
+    PrintLine("Hi there!");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
